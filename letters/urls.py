28867a91f354bd716a2b2c5389a5c6ecf88e20fb
yunheewon <yunheewon66@gmail.com>
2025-11-10 15:50:41 +0900

    fix-내게쓰는 편지와 수신함 분리

--- letters/urls.py
+++ letters/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.create_direct_letter),          # POST
     path("inbox/", views.direct_letter_inbox),     # GET 목록
+    path("self/", views.self_letter_inbox),  
     path("<int:id>/", views.direct_letter_detail), # GET 상세
 ]
